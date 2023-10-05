361f96c54a69d594b55d9cb5bd4bcdc3720e9ab4
Michał Boczula <Michal.Boczula@kruksa.pl>
2023-10-05 11:54:47 +0200

    Mocked products lists

--- Products/Application/Features/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
+++ Products/Application/Features/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Products.Application.Contracts.Persistance;
 using Products.Application.Features.Common;
+using Products.Domain.Entities;
 
 namespace Products.Application.Features.Queries.GetAllProducts
 {
@@ -16,7 +17,34 @@ namespace Products.Application.Features.Queries.GetAllProducts
         {
             try
             {
-                var products = await this._context.Products.ToListAsync();
+                //var products = await this._context.Products.ToListAsync();
+                var products = new List<Product>()
+                                   {
+                                       new Product
+                                           {
+                                               Id = 1,
+                                               Name = "Chinese",
+                                               ImgName = "Huawei",
+                                               Price = 2000,
+                                               IntegrationId = new Guid("55CCEE28-E15D-4644-A7BE-2F8A93568D6F")
+                                           },
+                                       new Product
+                                           {
+                                               Id = 2,
+                                               Name = "Samsung",
+                                               ImgName = "Samsung",
+                                               Price = 3000,
+                                               IntegrationId = new Guid("0EF1268E-33D6-49CD-A4B5-8EB94494D896")
+                                           },
+                                       new Product
+                                           {
+                                               Id = 3,
+                                               Name = "Iphone",
+                                               ImgName = "Iphone",
+                                               Price = 4000,
+                                               IntegrationId = new Guid("23363AFF-DD71-4F3C-8381-F7E71021761E")
+                                           }
+                                   };
                 var result = this._mapper.Map<List<ProductDto>>(products);
                 return new GetAllProductsQueryResult { Products = result, ErrorDescription = null };
             }
--- Products/Application/Features/Queries/GetProductsByIntegrationId/GetProductsByIntegrationIdQueryHandler.cs
+++ Products/Application/Features/Queries/GetProductsByIntegrationId/GetProductsByIntegrationIdQueryHandler.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Products.Application.Contracts.Persistance;
 using Products.Application.Features.Common;
+using Products.Domain.Entities;
 
 namespace Products.Application.Features.Queries.GetProductsByIntegrationId
 {
@@ -17,9 +18,34 @@ namespace Products.Application.Features.Queries.GetProductsByIntegrationId
         {
             try
             {
-                var products = await _context.Products
-                    .Where(x => request.ExternalContract.IntegrationIds.Contains(x.IntegrationId))
-                    .ToListAsync();
+                var prod = new List<Product>()
+                                              {
+                                                  new Product
+                                                      {
+                                                          Id = 1,
+                                                          Name = "Chinese",
+                                                          ImgName = "Huawei",
+                                                          Price = 2000,
+                                                          IntegrationId = new Guid("55CCEE28-E15D-4644-A7BE-2F8A93568D6F")
+                                                      },
+                                                  new Product
+                                                      {
+                                                          Id = 2,
+                                                          Name = "Samsung",
+                                                          ImgName = "Samsung",
+                                                          Price = 3000,
+                                                          IntegrationId = new Guid("0EF1268E-33D6-49CD-A4B5-8EB94494D896")
+                                                      },
+                                                  new Product
+                                                      {
+                                                          Id = 3,
+                                                          Name = "Iphone",
+                                                          ImgName = "Iphone",
+                                                          Price = 4000,
+                                                          IntegrationId = new Guid("23363AFF-DD71-4F3C-8381-F7E71021761E")
+                                                      }
+                                              };
+                var products = prod.Where(x => request.ExternalContract.IntegrationIds.Contains(x.IntegrationId));
                 var dtos = this._mapper.Map<List<ProductDto>>(products);
                 var result = this._mapper.Map<List<ProductExternal>>(dtos);
                 return new GetProductsByIntegrationIdQueryResult { Products = result, ErrorDescription = null };
